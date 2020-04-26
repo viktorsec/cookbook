0957a581ec0aeef323cfa650e490385a4ba49b11
Viktor Sec <hey@viktorsec.com>
2020-04-26 18:01:26 +0200

    Add list of Markdown files to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
 
-export default IndexPage
+export default ({ data }) => {
+  console.log(data)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello world</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{" "}
+            <span>
+              — {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+      <p>Total files: {data.allMarkdownRemark.totalCount}</p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/Šaltibarščiai.md
@@ -0,0 +1,8 @@
+---
+title: "Šaltibarščiai"
+date: "2020-04-26"
+---
+
+S touto obdobou Boršču som sa zoznámil v Pobaltí.
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/d/df/Chlodnik_%28Cold_Borscht%29.jpg" />
